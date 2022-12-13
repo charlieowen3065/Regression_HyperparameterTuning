7dca57ba98598cc7e99d64c87107f6c1733e207c
Charlie Owen <charlieowen3065>
2022-12-13 00:00:01 -0500

    Made a few changes to work in the test-train split.
     - added in the new variables from input_dict:
        - 'model_name'
        - 'test_train_split_var'
        - 'split_decimal'
        - 'tr_ts_seed'
     - Modified 'SVM_Linear' processing to only include C and Epsilon
     - Added a section at the end to get the hyperparameters from the best
       model

--- Regression_HPT_folder/function_files/data_processing.py
+++ Regression_HPT_folder/function_files/data_processing.py
@@ -2,6 +2,8 @@ import shutil
 import sys
 import os
 import pickle
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 # IMPORT FUNCTIONS FROM "FUNCTION_FILES"****************************************************************************** #
 pickle_file_decoded = []
@@ -28,6 +30,7 @@ X_input = np.genfromtxt('feature_data.csv', delimiter=',')
 Y_input = input_dict['Y_inp']
 model_use = input_dict['models_use']
 model_type = input_dict['model_type']
+model_name = input_dict['model_name']
 numLayers = input_dict['numLayers']
 numZooms = input_dict['numZooms']
 N = input_dict['N']
@@ -36,6 +39,10 @@ gridLength = input_dict['gridLength']
 seed = input_dict['seed']
 goodIds = input_dict['goodIDs']
 hyperparameters = input_dict['hyperparameters']
+
+test_train_split_var = input_dict['test_train_split_var']
+split_decimal = input_dict['split_decimal']
+tr_ts_seed = input_dict['tr_ts_seed']
 
 ht = heatmaps(X_input, Y_input,
               N=N, Nk=Nk,
@@ -51,9 +58,8 @@ os.chdir('Heatmaps')
 if model_use[0]:  # LINEAR
     C_input_data = hyperparameters['C']
     epsilon_input_data = hyperparameters['e']
-    gamma_input_data = hyperparameters['g']
 
-    ht.runFullGridSearch_SVM_C_Epsilon_Gamma(C_input_data, epsilon_input_data, gamma_input_data)
+    ht.runFullGridSearch_SVM_C_Epsilon(C_input_data, epsilon_input_data)
 
 elif model_use[1]:  # POLY-2
     C_input_data = hyperparameters['C']
@@ -108,5 +114,23 @@ elif model_use[7]:  # Matern 5/2
     length_input_data = hyperparameters['scale_length']
 
     ht.runFullGridSearch_GPR_Noise_SigF_Length(noise_input_data, sigF_input_data, length_input_data)
+
+if test_train_split_var:
+    sorted_data_df = pd.read_csv("0-Data_"+model_name+"_Sorted.csv")
+    best_model_data = sorted_data_df.iloc[0,:]
+    
+    if model_type == 'SVM':
+        C = best_model_data['C']
+        Epsilon = best_model_data['Epsilon']
+        Coef0 = best_model_data['Coef0']
+        Gamma = best_model_data['Gamma']
+    
+    if model_type == 'GPR':
+        if model_use[4]:
+            Alpha = best_model_data['Alpha']
+        Scale_Length = best_model_data['Length']
+        Noise = best_model_data['Noise']
+        Sigma_F = best_model_data['Sigma_F']
+
 
 os.chdir('..')
