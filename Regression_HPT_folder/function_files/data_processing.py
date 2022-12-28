ab4153d9ce46e29d0197b55d986aba4cd930477c
Charlie Owen <charlieowen3065>
2022-12-28 10:34:03 -0500

    Updated files

--- Regression_HPT_folder/function_files/data_processing.py
+++ Regression_HPT_folder/function_files/data_processing.py
@@ -1,7 +1,4 @@
 import random
-
-
-def process_test():
     import shutil
     import sys
     import os
@@ -226,87 +223,87 @@ def process_test():
         sigF_input_data = hyperparameters['sigmaF']
         length_input_data = hyperparameters['scale_length']
 
-        ht = heatmaps(X_input, Y_input, Nk=Nk, N=N,
-                      num_HP_zones_AL=num_HP_zones_AL, num_runs_AL=num_runs_AL,
-                      numLayers_GS=numLayers_GS, numZooms_GS=numZooms_GS,
-                      gridLength_AL=gridLength_AL, gridLength_GS=gridLength_GS,
-                      decimal_points_int=decimal_points_int, decimal_points_top=decimal_points_top,
-                      decimal_point_GS=decimal_point_GS,
-                      RemoveNaN=RemoveNaN_var, goodIDs=goodIds_var, seed=seed, models_use=model_use,
-                      save_csv_files=True,
-                      C_input='None', epsilon_input='None', gamma_input='None', coef0_input='None',
-                      noise_input=noise_input_data, sigmaF_input=sigF_input_data, length_input=length_input_data,
-                      alpha_input='None')
+    ht = heatmaps(X_input, Y_input, Nk=Nk, N=N,
+                  num_HP_zones_AL=num_HP_zones_AL, num_runs_AL=num_runs_AL,
+                  numLayers_GS=numLayers_GS, numZooms_GS=numZooms_GS,
+                  gridLength_AL=gridLength_AL, gridLength_GS=gridLength_GS,
+                  decimal_points_int=decimal_points_int, decimal_points_top=decimal_points_top,
+                  decimal_point_GS=decimal_point_GS,
+                  RemoveNaN=RemoveNaN_var, goodIDs=goodIds_var, seed=seed, models_use=model_use,
+                  save_csv_files=True,
+                  C_input='None', epsilon_input='None', gamma_input='None', coef0_input='None',
+                  noise_input=noise_input_data, sigmaF_input=sigF_input_data, length_input=length_input_data,
+                  alpha_input='None')
 
-        ht.runActiveLearning()
+    ht.runActiveLearning()
 
-    if test_train_split_var:
+if test_train_split_var:
+
+    if model_type == 'SVM':
+        col_names = ['RMSE', 'R2', 'Cor', 'C', 'Epsilon', 'Gamma', 'Coef0',
+                     'kF-Training-RMSE', 'kF-Training-R2', 'kF-Training-Cor',
+                     'Average-Training-to-Final-Training-Ratio']
+    elif model_type == 'GPR':
+        col_names = ['RMSE', 'R2', 'Cor', 'Noise', 'Length', 'SigmaF', 'Alpha',
+                     'kF-Training-RMSE', 'kF-Training-R2', 'kF-Training-Cor',
+                     'Average-Training-to-Final-Training-Ratio']
+
+    top_models_df = pd.DataFrame(columns=col_names)
+
+    for i in range(2):
+        sorted_data_df = pd.read_csv("0-Data_" + model_name + "_Sorted.csv")
+        best_model_data = sorted_data_df.iloc[i, :]
+
+        BM_RMSE = best_model_data['RMSE']
+        BM_R2 = best_model_data['R^2']
+        BM_Cor = best_model_data['Cor']
+        BM_avgTR_to_finalTR_Error = best_model_data['avgTR to Final Error']
 
         if model_type == 'SVM':
-            col_names = ['RMSE', 'R2', 'Cor', 'C', 'Epsilon', 'Gamma', 'Coef0',
-                         'kF-Training-RMSE', 'kF-Training-R2', 'kF-Training-Cor',
-                         'Average-Training-to-Final-Training-Ratio']
-        elif model_type == 'GPR':
-            col_names = ['RMSE', 'R2', 'Cor', 'Noise', 'Length', 'SigmaF', 'Alpha',
-                         'kF-Training-RMSE', 'kF-Training-R2', 'kF-Training-Cor',
-                         'Average-Training-to-Final-Training-Ratio']
+            C = best_model_data['C']
+            Epsilon = best_model_data['Epsilon']
+            Coef0 = best_model_data['Coef0']
+            Gamma = best_model_data['Gamma']
 
-        top_models_df = pd.DataFrame(columns=col_names)
+            Alpha = 1
+            Scale_Length = 1
+            Noise = 1
+            Sigma_F = 1
 
-        for i in range(2):
-            sorted_data_df = pd.read_csv("0-Data_" + model_name + "_Sorted.csv")
-            best_model_data = sorted_data_df.iloc[i, :]
-
-            BM_RMSE = best_model_data['RMSE']
-            BM_R2 = best_model_data['R^2']
-            BM_Cor = best_model_data['Cor']
-            BM_avgTR_to_finalTR_Error = best_model_data['avgTR to Final Error']
-
-            if model_type == 'SVM':
-                C = best_model_data['C']
-                Epsilon = best_model_data['Epsilon']
-                Coef0 = best_model_data['Coef0']
-                Gamma = best_model_data['Gamma']
-
+        if model_type == 'GPR':
+            if model_use[4]:
+                Alpha = best_model_data['Alpha']
+            else:
                 Alpha = 1
-                Scale_Length = 1
-                Noise = 1
-                Sigma_F = 1
+            Scale_Length = best_model_data['Length']
+            Noise = best_model_data['Noise']
+            Sigma_F = best_model_data['Sigma_F']
 
-            if model_type == 'GPR':
-                if model_use[4]:
-                    Alpha = best_model_data['Alpha']
-                else:
-                    Alpha = 1
-                Scale_Length = best_model_data['Length']
-                Noise = best_model_data['Noise']
-                Sigma_F = best_model_data['Sigma_F']
+            C = 1
+            Epsilon = 1
+            Coef0 = 1
+            Gamma = 1
 
-                C = 1
-                Epsilon = 1
-                Coef0 = 1
-                Gamma = 1
+        reg = Regression(X_test, Y_test, models_use=model_use, seed=seed, RemoveNaN=False,
+                         C=C, epsilon=Epsilon, gamma=Gamma, coef0=Coef0, noise=Noise, sigma_F=Sigma_F,
+                         scale_length=Scale_Length, alpha=Alpha)
 
-            reg = Regression(X_test, Y_test, models_use=model_use, seed=seed, RemoveNaN=False,
-                             C=C, epsilon=Epsilon, gamma=Gamma, coef0=Coef0, noise=Noise, sigma_F=Sigma_F,
-                             scale_length=Scale_Length, alpha=Alpha)
+        results = reg.Regression_test_multProp(X_train, X_test, Y_train, Y_test)
 
-            results = reg.Regression_test_multProp(X_train, X_test, Y_train, Y_test)
+        rmse_temp = results['metrics'].loc[model_name]["RMSE"]
+        r2_temp = results['metrics'].loc[model_name]["R2"]
+        cor_temp = results['metrics'].loc[model_name]["Cor"]
 
-            rmse_temp = results['metrics'].loc[model_name]["RMSE"]
-            r2_temp = results['metrics'].loc[model_name]["R2"]
-            cor_temp = results['metrics'].loc[model_name]["Cor"]
+        if model_type == 'SVM':
+            row_data = [rmse_temp, r2_temp, cor_temp, C, Epsilon, Gamma, Coef0, BM_RMSE, BM_R2, BM_Cor,
+                        BM_avgTR_to_finalTR_Error]
+        if model_type == 'GPR':
+            row_data = [rmse_temp, r2_temp, cor_temp, Noise, Scale_Length, Sigma_F, Alpha, BM_RMSE, BM_R2, BM_Cor,
+                        BM_avgTR_to_finalTR_Error]
 
-            if model_type == 'SVM':
-                row_data = [rmse_temp, r2_temp, cor_temp, C, Epsilon, Gamma, Coef0, BM_RMSE, BM_R2, BM_Cor,
-                            BM_avgTR_to_finalTR_Error]
-            if model_type == 'GPR':
-                row_data = [rmse_temp, r2_temp, cor_temp, Noise, Scale_Length, Sigma_F, Alpha, BM_RMSE, BM_R2, BM_Cor,
-                            BM_avgTR_to_finalTR_Error]
+        top_models_df.loc[str(i)] = row_data
 
-            top_models_df.loc[str(i)] = row_data
+    top_models_df = top_models_df.sort_values(by=["RMSE"], ascending=True)
+    top_models_df.to_csv('Best_Models.csv')
 
-        top_models_df = top_models_df.sort_values(by=["RMSE"], ascending=True)
-        top_models_df.to_csv('Best_Models.csv')
-
-    os.chdir('..')
+os.chdir('..')
